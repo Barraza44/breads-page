851e215cf1c3ce7e377069f59324f08d364fe6d0
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-08-04 23:51:37 -0500

    added validation rules

--- src/bread/dto/create-bread.dto.ts
+++ src/bread/dto/create-bread.dto.ts
@@ -1,9 +1,26 @@
 import { Ingredients } from "../Ingredients";
+import { IsString, Max, Min, IsNumber, IsUrl, Length, ValidateNested, MaxLength } from "class-validator";
 
 export class CreateBreadDto {
+  @IsString()
+  @Length(1, 100)
   name: string;
+
+  @IsNumber()
+  @Min(1)
+  @Max(999)
   price: number;
+
+  @IsString()
+  @Length(1, 3000)
   description: string;
+
+  @IsUrl()
   imageURI: string;
+
+  @ValidateNested()
+  @MaxLength(20, {
+    each: true
+  })
   ingredients: Ingredients[];
 }
