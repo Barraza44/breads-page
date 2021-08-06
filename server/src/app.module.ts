32211a3dea1dcc014b3a9e38e34c813388cdeb46
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-08-06 01:49:08 -0500

    restructured project

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,14 +5,11 @@ import { BreadModule } from './bread/bread.module';
 import { MongooseModule } from "@nestjs/mongoose";
 import {InvalidIdFilter} from "./filters/invalid-id.filter";
 import { APP_FILTER } from "@nestjs/core";
-import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
 
 @Module({
   imports: [
     BreadModule,
     MongooseModule.forRoot('mongodb://nesty:nestymn4@localhost:27017/Bread?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'),
-    ServeStaticModule.forRoot({ rootPath: join(__dirname, "..", "client"), exclude: ['/api*']})
   ],
   controllers: [AppController],
   providers: [
