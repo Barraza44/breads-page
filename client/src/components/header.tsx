7c012713aebb23e8fdb4f6ef1f3c0275d7767f6f
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-09-12 20:59:58 -0500

    add background color change on scroll

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import Bag from "../icons/bag-outline.svg";
+import { motion } from "framer-motion";
 
-const Header: React.FC = () => {
+const Header: React.FC<headerProps> = ({inView}: headerProps) => {
+  const [background, setBackground] = useState("rgba(0, 0, 0, 0)");
+
+  useEffect(() => {
+   if (inView) {
+     setBackground("rgba(255, 255, 255, 1)")
+   } else {
+     setBackground("rgba(0, 0, 0, 0)")
+   }
+  }, [inView])
+
+  
   return (
-    <nav className="flex flex-row justify-between text-4xl p-4">
+    <motion.nav
+      className={"flex flex-row justify-between w-full text-4xl p-4 fixed top-0 z-10 transition duration-150 ease-in-out"}
+      style={{backgroundColor: background}}
+    >
       <h1 className="font-main ml-4 md:text-5xl">
         <Link to="/">Lolita's</Link>
       </h1>
       <img src={Bag} alt="A shopping bag" className="h-7 md:h-10 self-center"/>
-    </nav>
+    </motion.nav>
   )
 }
 
 export default Header;
+
+interface headerProps {
+  inView: boolean
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,13 +4,15 @@ import Bread from "../images/breads.jpg";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import cardData from "../cardData";
+import { useInView } from "react-intersection-observer";
 
 const Index: React.FC = () => {
+  const [ref, inView] = useInView({threshold: 0.35});
   return (
-    <div className="bg-gradient-to-br from-beige-light to-beige-dark h-screen lg:h-screen">
-      <Header/>
-      <main className="p-4 md:p-8 lg:mt-16 flex flex-col justify-center items-center lg:h-4/6">
-        <div className="flex flex-col justify-center h-full mb-20 items-center lg:grid lg:grid-cols-2 lg:grid-rows-3 lg:mt-44">
+    <>
+      <Header inView={inView}/>
+      <main className="flex flex-col justify-center items-center lg:h-4/6 z-0">
+        <section className="p-4 md:p-8 bg-gradient-to-br from-beige-light to-beige-dark flex flex-col justify-center h-full md:h-screen w-full mb-20 items-center lg:grid lg:grid-cols-2 lg:grid-rows-3">
           <h1 className="font-main text-6xl text-center col-start-1 col-end-2 font-bold lg:text-left lg:text-8xl">
             Panes para todos los <span className="font-main text-pinky">gustos</span>
           </h1>
@@ -24,7 +26,8 @@ const Index: React.FC = () => {
           <Button size="w-64 lg:w-72 lg:h-14" layout="row-start-3 lg:mb-30">
             Compra ahora
           </Button>
-        </div>
+        </section>
+        <section className="p-4 md:p-8" ref={ref}>
         <h2 className="text-4xl lg:mt-0 text-center font-main">¿Por qué Lolita's?</h2>
         <p className="text-xl mt-6 font-secondary">Descubre por que somos la elección favorita de nuestros clientes.</p>
         <div className="flex flex-col gap-10 md:flex-row ">
@@ -37,8 +40,9 @@ const Index: React.FC = () => {
               alt={item.alt}/>
           ))}
         </div>
+        </section>
       </main>
-    </div>
+    </>
   );
 }
 
