15b3b197199b777901c7fa9c33f8052e7172a48e
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-09-12 21:48:06 -0500

    add box-shadow toggle

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -5,19 +5,22 @@ import { motion } from "framer-motion";
 
 const Header: React.FC<headerProps> = ({inView}: headerProps) => {
   const [background, setBackground] = useState("rgba(0, 0, 0, 0)");
+  const [shadow, setShadow] = useState("")
 
   useEffect(() => {
    if (inView) {
-     setBackground("rgba(255, 255, 255, 1)")
+     setBackground("rgba(255, 255, 255, 1)");
+     setShadow("shadow-lg");
    } else {
-     setBackground("rgba(0, 0, 0, 0)")
+     setBackground("rgba(0, 0, 0, 0)");
+     setShadow("");
    }
   }, [inView])
 
   
   return (
     <motion.nav
-      className={"flex flex-row justify-between w-full text-4xl p-4 fixed top-0 z-10 transition duration-150 ease-in-out"}
+      className={`flex flex-row justify-between w-full text-4xl p-4 fixed top-0 z-10 transition ${shadow} duration-150 ease-in-out`}
       style={{backgroundColor: background}}
     >
       <h1 className="font-main ml-4 md:text-5xl">
